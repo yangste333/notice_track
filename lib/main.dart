56c97c355fa431d57477ef3cacc8cfe5809d940b
yangste333 <yangste333@gmail.com>
2024-05-08 16:46:42 -0700

    created basic settings page
    created settings page based on local storage through hive, will need to add all fields for adjusted settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notice_track/database/firebase_options.dart';
 import 'package:notice_track/app.dart';
+import 'package:notice_track/user_settings.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserSettingsAdapter());
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
+
